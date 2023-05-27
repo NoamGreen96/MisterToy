83d9981803804f707f36b00209abfc329ebd0f0b
Noam Green <Noamgr11@gmail.com>
2023-05-27 17:54:11 +0300

    server config for hosting

--- backend/server.js
+++ backend/server.js
@@ -6,19 +6,28 @@ const cookieParser = require('cookie-parser')
 const path = require('path')
 const cors = require('cors')
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+    const corsOptions = {
+        origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
+}
 
 // App Configuration
-const corsOptions = {
-    origin: [
-        'http://127.0.0.1:8080',
-        'http://localhost:8080',
-        'http://127.0.0.1:3000',
-        'http://localhost:3000'
-    ],
-    credentials: true
-}
-app.use(cors(corsOptions))
-app.use(express.static('public'))
+// const corsOptions = {
+//     origin: [
+//         'http://127.0.0.1:8080',
+//         'http://localhost:8080',
+//         'http://127.0.0.1:3000',
+//         'http://localhost:3000'
+//     ],
+//     credentials: true
+// }
+// app.use(cors(corsOptions))
+// app.use(express.static('public'))
 app.use(cookieParser()) // for res.cookies
 app.use(express.json()) // for req.body
 
@@ -122,4 +131,11 @@ app.delete('/api/toy/:toyId', (req, res) => {
 
 
 // Listen will always be the last line in our server!
-app.listen(3030, () => console.log('Server listening on port 3030!'))
+// app.listen(3030, () => console.log('Server listening on port 3030!'))
+const port = process.env.PORT || 3030;
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+app.listen(port, () => {
+    console.log(`App listening on port ${port}!`)
+});
